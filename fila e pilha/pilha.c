f3aa53fc42d6b368ea70e04edd24582171e9161b
Victor Lemos <49991251+grimmer10u@users.noreply.github.com>
2019-10-16 09:28:37 -0300

    pilha base

---
+++ fila e pilha/pilha.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define max 100
+
+typedef struct{
+    int topo;
+    int vet[max];
+} pilha;
+
+pilha *cria_pilha(){
+    pilha p* = (pilha*)malloc(sizeof(pilha));
+    p -> topo = 0;
+}
+
+int push(pilha *p, int v){
+    if(p -> topo < max){
+        p -> vet[p -> topo] = v;
+        p -> topo++;
+    }
+}
+
+int pop(pilha *p){
+    int ret;
+    if(p -> topo > 0){
+        aux = p -> vet[p -> topo-1];
+        p -> topo--;
+    }
+    return ret;
+}
+
+int size(pilha *p){
+    return p -> topo;
+}
+
+int is_empty(pilha *p){
+    return p -> topo == 0;
+}
+
+int is_full(pilha *p){
+    return p -> topo == max;
+}
+
+int main(void)
+{
+    
+    return 0;
+}
