04f2ecb7dd3065590c12caae97a2675073e76ef0
Victor Lemos <danivictor45i@gmail.com>
2019-10-31 19:27:41 +0000

    list

--- listaencadeada/listaencadeada.c
+++ listaencadeada/listaencadeada.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "listaencadeada.h"
+#include <stdio.h>
 
 list *createList(){
      list *l = (list*)malloc(sizeof(list));
@@ -21,7 +22,32 @@ void add(list *l, int v){
         l -> begin = novo;
     }
     else{
-        a -> next = n;
+        a -> next = novo;
         free(p);
+    }
+}
+
+void printList(list *l){
+    printf("{");
+    node *p = l -> begin;
+    while(p != NULL){
+        if(p -> next != NULL) printf("%d ", p -> data);
+        else printf("%d", p -> data);
+        p = p -> next;
+    }
+    printf("}");
+}
+
+int isEmpty(list *l){
+    if(l -> begin == NULL) return 1;
+    else return 0;
+}
+
+void removeBack(list *l){
+    node *p = l -> begin;
+    node *a = NULL;
+    while(p != NULL && p -> next != NULL){
+        a = p;
+        p = p -> next;
     }
 }
