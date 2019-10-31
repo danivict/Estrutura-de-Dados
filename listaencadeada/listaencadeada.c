1876ca40e0c4f46f33e688e430e52cb4b6d76410
Victor Lemos <danivictor45i@gmail.com>
2019-10-31 17:57:52 +0000

    list

--- listaencadeada/listaencadeada.c
+++ listaencadeada/listaencadeada.c
@@ -4,6 +4,24 @@
 list *createList(){
      list *l = (list*)malloc(sizeof(list));
     l -> begin = NULL;
-     return lista;
+     return l;
 }
 
+void add(list *l, int v){
+    node *novo = (node*)malloc(sizeof(node));
+    novo -> data = v;
+    novo -> next = NULL;
+    node *p = l -> begin;
+    node *a = NULL;
+    while(p != NULL){
+        a = p;
+        p = p -> next;
+    }
+    if (a == NULL){
+        l -> begin = novo;
+    }
+    else{
+        a -> next = n;
+        free(p);
+    }
+}
