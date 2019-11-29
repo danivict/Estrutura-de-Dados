14c7887f9b8b9964e511974ac592ad2af50f804e
daniel <danivictor45i@gmail.com>
2019-11-28 23:25:21 -0200

    correcao

--- fila e pilha/pilha.c
+++ fila e pilha/pilha.c
@@ -2,46 +2,55 @@
 
 #define max 100
 
-typedef struct{
+typedef struct
+{
     int topo;
     int vet[max];
 } pilha;
 
-pilha *cria_pilha(){
-    pilha p* = (pilha*)malloc(sizeof(pilha));
-    p -> topo = 0;
+pilha *cria_pilha()
+{
+    pilha *p = (pilha *)malloc(sizeof(pilha));
+    p->topo = 0;
 }
 
-int push(pilha *p, int v){
-    if(p -> topo < max){
-        p -> vet[p -> topo] = v;
-        p -> topo++;
+int push(pilha *p, int v)
+{
+    if (p->topo < max)
+    {
+        p->vet[p->topo] = v;
+        p->topo++;
     }
 }
 
-int pop(pilha *p){
+int pop(pilha *p)
+{
     int ret;
-    if(p -> topo > 0){
-        aux = p -> vet[p -> topo-1];
-        p -> topo--;
+    if (p->topo > 0)
+    {
+        int aux = p->vet[p->topo - 1];
+        p->topo--;
     }
     return ret;
 }
 
-int size(pilha *p){
-    return p -> topo;
+int size(pilha *p)
+{
+    return p->topo;
 }
 
-int is_empty(pilha *p){
-    return p -> topo == 0;
+int is_empty(pilha *p)
+{
+    return p->topo == 0;
 }
 
-int is_full(pilha *p){
-    return p -> topo == max;
+int is_full(pilha *p)
+{
+    return p->topo == max;
 }
 
 int main(void)
 {
-    
+
     return 0;
 }
